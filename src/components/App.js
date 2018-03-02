64f9660f5f26fd984a9377a6d92c1553f4bf72bb
Mai Đức Thắng <maiducthang97@gmail.com>
2018-03-02 14:56:34 +0700

    a

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/decistyle.css';
+import '../style/poliaStyle.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
